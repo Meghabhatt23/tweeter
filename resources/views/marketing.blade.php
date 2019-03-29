51cfda8003accc4e901b62cc2be31b3813321c44
Meghabhatt23 <44779627+Meghabhatt23@users.noreply.github.com>
2019-03-29 09:43:20 -0600

    marketing slises

--- resources/views/marketing.blade.php
+++ resources/views/marketing.blade.php
@@ -20,7 +20,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
 <div class="container">
 	<div class="row heading-box">
-		<h2 class="text-center">Bootstrap 4 slider with Text Animation</h2>
+		<h2 class="text-center">7 Reasons Why You Should Be Using Tweeter! </h2>
 	</div>
 </div>
 <section>
@@ -36,26 +36,55 @@
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://i.pinimg.com/originals/10/1b/bb/101bbb8a4e78d2a5d9589d60e410dac8.jpg" alt="Los Angeles">
+      <img src="https://techcrunch.com/wp-content/uploads/2018/07/fake-twitter-accounts.png?w=730&crop=1"  style="width:100%;height:100%;"alt="Los Angeles">
         <div class="text-box">
-            <h2 class="wow slideInRight" data-wow-duration="2s">This is Obitope text</h2>
-            <p class="wow slideInLeft" data-wow-duration="2s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
+            <h2 class="wow slideInRight" data-wow-duration="2s">What is Tweeter?</h2>
+            <p class="wow slideInLeft" data-wow-duration="2s">Twitter is an online news and social networking site where people communicate in short messages called tweets. Tweeting is posting short messages for anyone who follows you on Twitter, with the hope that your messages are useful and interesting to someone in your audience </p>
         </div>
     </div>
     <div class="carousel-item">
-      <img src="http://en.ddpai.com/webcommon/images/mix3/animation/bac-2.png" alt="Chicago">
-    <div class="text-box">
-            <h2 class="wow slideInUp" data-wow-duration="4s" >This is samuel text</h2>
-            <p class="wow fadeInDown" data-wow-duration="4s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
+      <img src="https://assets.entrepreneur.com/content/3x2/2000/20170926163842-GettyImages-543082622.jpeg?width=700&crop=2:1"  style="width:100%;height:100%;" alt="Chicago">
+    <div class="text-box" style="color:white; font-weight:bold;">
+            <h2 class="wow slideInUp" data-wow-duration="4s" >Interesting People</h2>
+            <p class="wow fadeInDown" data-wow-duration="4s">Twitter is meant not for friends and family but for people you actually want to communicate with. You can follow anyone and anyone can follow you. </p>
         </div>
     </div>
     <div class="carousel-item">
-      <img src="https://playthunee.com/assets/images/4player-bg.jpg" alt="New York">
+      <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2019/01/10-Reasons-Why-Social-Media-Is-Important-to-Your-Company.png" style="width:100%;height:100%;" alt="New York">
     <div class="text-box">
-            <h2 class="wow fadeInUp" data-wow-duration="4s">This is Airborne text</h2>
-            <p class="wow fadeInUp" data-wow-duration="2s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
+            <h2 class="wow fadeInUp" data-wow-duration="4s">Company Contact</h2>
+            <p class="wow fadeInUp" data-wow-duration="2s">Twitter represents one of the quickest and easiest ways to contact a company to get instant customer service. The response you receive may not be as fine-tuned as you would get by speaking to a person on the phone, but at least you won’t be kept on hold for hours before you get that attention. </p>
         </div>
     </div>
+    <div class="carousel-item">
+      <img src="http://waarmedia.com/english/file/2017/02/Breaking-News-1.jpg" style="width:100%;height:100%;" alt="New York">
+    <div class="text-box">
+            <h2 class="wow fadeInUp" data-wow-duration="4s">Breaking News</h2>
+            <p class="wow fadeInUp" data-wow-duration="2s">TTwitter has proved its worth in recent years as a platform for breaking news. The simplicity of the site means that the first think many people at the scene of a developing news story do is to tweet about it. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://ichef.bbci.co.uk/images/ic/960x540/p06qmy87.jpg" style="width:100%;height:100%;" alt="New York">
+    <div class="text-box">
+            <h2 class="wow fadeInUp" data-wow-duration="4s">Celebrity Access</h2>
+            <p class="wow fadeInUp" data-wow-duration="2s">While those who are really keen on certain famous people have always found ways to talk to them, Twitter makes it very simple indeed. If a celebrity is on Twitter then anyone else on Twitter can send a message to them. They may even respond. Indeed, I’ve communicated with several celebrities on Twitter in this way. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="http://gobrandgo.com/wp-content/uploads/2013/05/hashtags.png" style="width:100%;height:100%;" alt="New York">
+    <div class="text-box">
+            <h2 class="wow fadeInUp" data-wow-duration="4s">Tracking Trends</h2>
+            <p class="wow fadeInUp" data-wow-duration="2s">Hashtags also figure into this, and most of the Trends listed on Twitter use this simple method of assigning a particular subject to a tweet. You can tailor Twitter trends to suit you, switching from ‘Worldwide’ to those emerging from a particular country. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.ytimg.com/vi/aH2ll5bwpKw/maxresdefault.jpg" style="width:100%;height:100%;" alt="New York">
+    <div class="text-box">
+            <h2 class="wow fadeInUp" data-wow-duration="4s">Creative Outlet</h2>
+            <p class="wow fadeInUp" data-wow-duration="2s"> Be imaginative with themed tweets!You may want to parody a famous person, write short poetry, recount witty anecdotes, or tell lame jokes. Twitter gives you a certain sense of anonymity to plumb the depths of your creativity. </p>
+        </div>
+    </div>
+
   </div>
 
   <!-- Left and right controls -->
