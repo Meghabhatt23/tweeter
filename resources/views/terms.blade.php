be8c845c2d83d72c53c48ac52e234c6483f27307
Meghabhatt23 <44779627+Meghabhatt23@users.noreply.github.com>
2019-04-01 08:56:32 -0600

    marketing page 90%

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -7,39 +7,127 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>terms</title>
+
 </head>
 <body>
+
+
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
+
+
 
 <div class="has-animation animation-ltr" data-delay="10" style="text-align:center;">
-  <p class="bigger" style="text-align:center;"> Why Tweeter? </p>
+  <p class="bigger" > Why Tweeter? </p>
 </div>
 
 <br>
+
+
+<div class="has-animation animation-rtl" data-delay="1000">
+  <h5 class="text">Twitter has now been with us for seven years and counting, having celebrated its seventh birthday on March 21, 2013. This was seven years to the day since Jack Dorsey sent out the first tweet in 2006, at a time when the micro-blogging social network was known simply as twttr.
+
+Twitter was originally an internal service for employees of Odeo but entered into the public domain in July 2006. By 2007 early adopters and tech bloggers had started to use Twitter, and the service has grown every year since then. In 2009 it benefited from the Oprah Effect, hitting the mainstream in a big, bad way. Those who are interested can read Twitter’s full origin story.
+
+How Twitter Was Born.
+</h5>
+</div>
 <br>
 
-<div class="auto-container">
-			<div class="row">
-				<div class="column col-lg-6 col-md-12 col-sm-12">
-					<div class="sec-title">
-						<h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
-					</div>
-				</div>
-			</div>
-		</div>	
-<br>
-<br>
+<section>
+    <div id="slider-animation" class="carousel slide" data-ride="carousel">
 
-<br>
-<br>
-<div class="has-animation animation-rtl" data-delay="2000">
-  <img src="https://placeimg.com/640/480/nature" width="600" />
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#slider-animation" data-slide-to="0" class="active"></li>
+    <li data-target="#slider-animation" data-slide-to="1"></li>
+    <li data-target="#slider-animation" data-slide-to="2"></li>
+    <li data-target="#slider-animation" data-slide-to="3"></li>
+    <li data-target="#slider-animation" data-slide-to="4"></li>
+    <li data-target="#slider-animation" data-slide-to="5"></li>
+    <li data-target="#slider-animation" data-slide-to="6"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner" style="margin-left:80px; margin-right:80px;">
+    <div class="carousel-item active">
+      <img src="https://techcrunch.com/wp-content/uploads/2018/07/fake-twitter-accounts.png?w=730&crop=1"  style="width:90%;"alt="Los Angeles">
+        <div class="text-box">
+            <h2 class="wow slideInRight" data-wow-duration="2s">What is Tweeter?</h2>
+            <p class="wow slideInLeft" data-wow-duration="2s">Twitter is an online news and social networking site where people communicate in short<br /> messages called tweets.Tweeting is posting short messages for anyone who follows you on Twitter,<br > with the hope that your messages are useful and interesting to someone in your audience </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://assets.entrepreneur.com/content/3x2/2000/20170926163842-GettyImages-543082622.jpeg?width=700&crop=2:1"  style="width:90%;height:70%;" alt="Chicago">
+    <div class="text-box" style="color:white; font-weight:bold;">
+            <h3 class="wow slideInUp" data-wow-duration="2s" >Interesting People</h3>
+            <p class="wow fadeInDown" data-wow-duration="2s">Twitter is meant not for friends and family but for people you actually want to communicate with. You can follow anyone and anyone can follow you. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2019/01/10-Reasons-Why-Social-Media-Is-Important-to-Your-Company.png" style="width:90%;height:70%;" alt="New York">
+    <div class="text-box" style="top:30%; font-weight:bold;">
+            <h3 class="wow fadeInUp" data-wow-duration="2s">Company Contact</h3>
+            <p class="wow fadeInUp" data-wow-duration="2s">Twitter represents one of the quickest and easiest ways to contact a company to get instant customer service. The response you receive may not be as fine-tuned as you would get by speaking to a person on the phone, but at least you won’t be kept on hold for hours before you get that attention. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="http://waarmedia.com/english/file/2017/02/Breaking-News-1.jpg" style="width:90%;height:70%;" alt="New York">
+    <div class="text-box" style="color:white; font-weight:bold; top:80%; font-weight:bold;">
+            <p class="wow fadeInUp" data-wow-duration="2s">Twitter has proved its worth in recent years as a platform for breaking news. The simplicity of the site means that the first think many people at the scene of a developing news story do is to tweet about it. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://ichef.bbci.co.uk/images/ic/960x540/p06qmy87.jpg" style="width:90%;height:70%; color:white;" alt="New York">
+    <div class="text-box" style="color:white; font-weight:bold;">
+        <h3 class="wow slideInUp" data-wow-duration="5s" >Celebrity Access</h3>
+            <p class="wow fadeInDown" data-wow-duration="5s" >While those who are really keen on certain famous people have always found ways to talk to them, Twitter makes it very simple indeed. If a celebrity is on Twitter then anyone else on Twitter can send a message to them. They may even respond. Indeed, I’ve communicated with several celebrities on Twitter in this way. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="http://gobrandgo.com/wp-content/uploads/2013/05/hashtags.png" style="width:90%;top:80%;" alt="New York">
+    <div class="text-box" style="color:black; font-weight:bold; top:70%; font-weight:bold;">
+            <h3 class="wow fadeInUp" data-wow-duration="2s">Tracking Trends</h3>
+            <p class="wow fadeInUp" data-wow-duration="2s">Hashtags also figure into this, and most of the Trends listed on Twitter use this simple method of assigning a particular subject to a tweet. You can tailor Twitter trends to suit you, switching from ‘Worldwide’ to those emerging from a particular country. </p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.ytimg.com/vi/aH2ll5bwpKw/maxresdefault.jpg" style="width:90%;height:70%;" alt="New York">
+    <div class="text-box"  style="color:black; font-weight:bold;">
+            <h3 class="wow fadeInUp" data-wow-duration="2s">Creative Outlet</h3>
+            <p class="wow fadeInUp" data-wow-duration="2s"> Be imaginative with themed tweets!You may want to parody a famous person, write short poetry, recount witty anecdotes, or tell lame jokes. Twitter gives you a certain sense of anonymity to plumb the depths of your creativity. </p>
+        </div>
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#slider-animation" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#slider-animation" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
 </div>
 
+</section>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://unpkg.com/splitting/dist/splitting.min.js"></script>
         <script type="text/javascript" src="{{ URL::asset('js/terms.js')}}"></script>
 </body>
 </html>
